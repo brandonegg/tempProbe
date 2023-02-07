34501dc0190de127349d02f61d599c85f63b53c0
Brandon Egger <brandonegger64@gmail.com>
2023-02-06 22:44:36 -0600

    add plotly graph

--- temp_monitor/main.py
+++ temp_monitor/main.py
@@ -1,28 +1,13 @@
+import plotly.express as px
+
 import flet as ft
+from flet.plotly_chart import PlotlyChart
 
 def main(page: ft.Page):
-    page.title = "Flet counter example"
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+    df = px.data.gapminder().query("continent=='Oceania'")
+    fig = px.line(df, x="year", y="lifeExp", color="country")
 
-    def minus_click(e):
-        txt_number.value = str(int(txt_number.value) - 1)
-        page.update()
-
-    def plus_click(e):
-        txt_number.value = str(int(txt_number.value) + 1)
-        page.update()
-
-    page.add(
-        ft.Row(
-            [
-                ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
-                txt_number,
-                ft.IconButton(ft.icons.ADD, on_click=plus_click),
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-        )
-    )
+    page.add(PlotlyChart(fig, expand=True))
 
 ft.app(target=main)
