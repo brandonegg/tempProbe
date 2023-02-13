b83020d236ba1837488cb319e29533408285ee85
Brandon Egger <brandonegger64@gmail.com>
2023-02-13 08:45:57 -0600

    added example input

--- temp_monitor/main.py
+++ temp_monitor/main.py
@@ -10,9 +10,17 @@ def main(page: ft.Page):
     '''
     Main page display
     '''
+    def test(e):
+        print("hello")
+
+    page.title('Temperature Monitor')
+
     d_f = px.data.gapminder().query("continent=='Oceania'")
     fig = px.line(d_f, x="year", y="lifeExp", color="country")
 
     page.add(PlotlyChart(fig, expand=True))
+
+    new_task = ft.TextField(hint_text="test", width=300)
+    page.add(ft.Row([new_task, ft.ElevatedButton("test", on_click=test)]))
 
 ft.app(target=main)
