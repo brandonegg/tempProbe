c64309fc40ec39aad687d3f5ec53373605a45f2c
Brandon Egger <brandonegger64@gmail.com>
2023-02-18 14:13:04 -0600

    setup platformio for esp32

--- temp_box/src/main.cpp
+++ temp_box/src/main.cpp
@@ -27,95 +27,13 @@
    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-#include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
-#include <StreamString.h>
-
-#ifndef STASSID
-#define STASSID "BeanNet"
-#define STAPSK "69BeansInMyMouth"
-#endif
-
-const char *ssid = STASSID;
-const char *password = STAPSK;
-
-ESP8266WebServer server(80);
-
-const int led = 13;
-
-void handleNotFound() {
-  digitalWrite(led, 1);
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-
-  for (uint8_t i = 0; i < server.args(); i++) { message += " " + server.argName(i) + ": " + server.arg(i) + "\n"; }
-
-  server.send(404, "text/plain", message);
-  digitalWrite(led, 0);
-}
-
-void drawGraph() {
-  String out;
-  out.reserve(2600);
-  char temp[70];
-  out += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"400\" height=\"150\">\n";
-  out += "<rect width=\"400\" height=\"150\" fill=\"rgb(250, 230, 210)\" stroke-width=\"1\" stroke=\"rgb(0, 0, 0)\" />\n";
-  out += "<g stroke=\"black\">\n";
-  int y = rand() % 130;
-  for (int x = 10; x < 390; x += 10) {
-    int y2 = rand() % 130;
-    sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" />\n", x, 140 - y, x + 10, 140 - y2);
-    out += temp;
-    y = y2;
-  }
-  out += "</g>\n</svg>\n";
-
-  server.send(200, "image/svg+xml", out);
-}
-
-void returnTempData() {
-  String out = "{\"past_300\": [10,20,30,40,50,60,70,80]}";
-  server.send(404, "application/json", out);
-}
+#include <Arduino.h>
 
 void setup(void) {
-  pinMode(led, OUTPUT);
-  digitalWrite(led, 0);
-  Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  Serial.println("");
-
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  if (MDNS.begin("esp8266")) { Serial.println("MDNS responder started"); }
-
-  server.on("/history", returnTempData);
-  server.onNotFound(handleNotFound);
-  server.begin();
-  Serial.println("HTTP server started");
+  Serial.begin(9600);
 }
 
 void loop(void) {
-  server.handleClient();
-  MDNS.update();
+  Serial.println("hello");
+  delay(100);
 }
