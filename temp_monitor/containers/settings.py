f4190742d4820ddaf11ce36f853c500735d31f0e
Brandon Egger <brandonegger64@gmail.com>
2023-02-18 15:29:35 -0600

    added phone number and min/max temperature input fields in settings

--- temp_monitor/containers/settings.py
+++ temp_monitor/containers/settings.py
@@ -17,4 +17,33 @@ class SettingsContainer(ft.Container):
         super().__init__(*args, **kwargs)
         self.app = app
         self.page = page
-        self.content = ft.Text('TODO')
+        self._add_settings()
+
+    def _add_settings(self):
+        self.phone_input = ft.TextField(label="Phone Number", hint_text='include area code', value="", text_align=ft.TextAlign.RIGHT, width=300)
+        self.min_temp_input = ft.TextField(label="Min", expand=True)
+        self.max_temp_input = ft.TextField(label="Max", expand=True)
+        self.update_button = ft.OutlinedButton(text="update")
+
+        self.content = ft.Container(
+            ft.Column(
+                [
+                    self.phone_input,
+                    ft.Text("Temperature Alert Triggers:"),
+                    ft.Row(
+                        [
+                            self.min_temp_input,
+                            self.max_temp_input
+                        ],
+                        width=200,
+                        spacing=20,
+                    ),
+                    ft.Container(
+                        self.update_button,
+                        alignment=ft.alignment.center,
+                    )
+                ],
+                width=200, 
+            ),
+            padding=30,
+        ) 
