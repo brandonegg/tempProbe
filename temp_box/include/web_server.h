434b41ca4113f9aae0262e7a0641f416a087bcda
Brandon Egger <brandonegger64@gmail.com>
2023-02-19 10:11:42 -0600

    add get phone number data route

--- temp_box/include/web_server.h
+++ temp_box/include/web_server.h
@@ -2,16 +2,41 @@
 #define TEMPERATURE_SERVER
 #include <WebServer.h>
 
+class PhoneAlertData {
+    public:
+        String phone_number = "";
+        int min_temp = 0;
+        int max_temp = 100;
+        String* format_json();
+};
+
 class TemperatureServer {
 
     public:
         TemperatureServer();
         static void handle_client();
 
+        int temp_buffer[300] = { };
+
     private:
         static WebServer *server;
-        static void handle_hello();
+        static PhoneAlertData* phone_alert;
+        char reference = 'c'; // C or F
+        
+        // Temperature Alert CRUD
+        static void handle_get_alert_data();
+
         static void handle_not_found();
 };
+
+/*
+class FlashManager {
+    public:
+        FlashManager();
+        char load_reference_mode();
+        store_reference_mode(char val);
+        store_phone_data(*PhoneAlert phone_data);
+}
+*/
 
 #endif
--- temp_box/src/web_server.cpp
+++ temp_box/src/web_server.cpp
@@ -2,13 +2,20 @@
 #include "web_server.h"
 #include <ESPmDNS.h>
 
+/*
+ * TEMPERATURE SERVER FUNCTIONS
+ */
 WebServer* TemperatureServer::server;
+PhoneAlertData* TemperatureServer::phone_alert;
 
 TemperatureServer::TemperatureServer() {
+    TemperatureServer::phone_alert = new PhoneAlertData();
+
     MDNS.begin("tempbox");
     TemperatureServer::server = new WebServer(80);
     // Routes
-    TemperatureServer::server->on("/hello", HTTP_GET, handle_hello);
+    TemperatureServer::server->on("/ping", HTTP_GET, handle_ping);
+    TemperatureServer::server->on("/alert", HTTP_GET, handle_get_alert_data);
     TemperatureServer::server->onNotFound(handle_not_found);
     // Start server
     TemperatureServer::server->begin();
@@ -16,8 +23,11 @@ TemperatureServer::TemperatureServer() {
     Serial.println("Web Server has successfully started!");
 }
 
-void TemperatureServer::handle_hello() {
-    TemperatureServer::server->send(200, "text/plain", "hello world");
+void TemperatureServer::handle_get_alert_data() {
+    Serial.println("Collecting data");
+    String* data = TemperatureServer::phone_alert->format_json();
+    TemperatureServer::server->send(200, "application/json", (*data));
+    delete data;
 }
 
 void TemperatureServer::handle_client() {
@@ -26,4 +36,12 @@ void TemperatureServer::handle_client() {
 
 void TemperatureServer::handle_not_found() {
     TemperatureServer::server->send(404, "text/plain", "Not found");
+}
+
+/*
+ * PHONE ALERT FUNCTIONS
+ */
+String* PhoneAlertData::format_json() {
+    String* response = new String("{\"phone_number\":\"" + phone_number + "\",\"min_temp\":" + String(min_temp) + ",\"max_temp\":" + String(max_temp) + "}");
+    return response;
 }
