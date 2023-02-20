d6017eb067d3f83cb8f026b87711c52d369dda45
Brandon Egger <brandonegger64@gmail.com>
2023-02-19 18:22:27 -0600

    made graph position moveable with trackpad

--- temp_monitor/containers/monitor.py
+++ temp_monitor/containers/monitor.py
@@ -24,7 +24,19 @@ class MonitorContainer(ft.Container):
         self.app = app
         self.page = page
         self.graph = TemperatureGraph(expand=True)
-        self.content = ft.Row([self.graph])
+
+        graph_gesture_wrapper = ft.GestureDetector(
+            mouse_cursor=ft.MouseCursor.MOVE,
+            on_scale_update=lambda event: self._scale_update(event),
+            content=self.graph,
+        )
+
+        self.content = ft.Row([graph_gesture_wrapper])
+
+    def _scale_update(self, event):
+        scale_factor = 2
+        self.graph.shift_axis(scale_factor*event.focal_point_delta_x, scale_factor*event.focal_point_delta_y)
+        self.page.update()
 
 class TemperatureGraph(PlotlyChart):
     '''
@@ -36,13 +48,11 @@ class TemperatureGraph(PlotlyChart):
     ):
         super().__init__(*args, **kwargs)
 
+        self.display_in = 'F'
         self._update_history()
 
-        self.display_in = 'F'
         self.figure = px.line(self.data, x="Time - Seconds", y=f"Temperature({self.display_in})")
         self.figure['layout']['xaxis']['autorange'] = "reversed"
-        # attach the call back
-        #self.figure.canvas.mpl_connect('scroll_event', lambda event: self.zoom_fun(event))
 
     def _update_history(self):
         '''
@@ -57,3 +67,15 @@ class TemperatureGraph(PlotlyChart):
   
             self.data.loc[:, "Temperature(F)"] = self.temp_history["f"]
             self.data.loc[:, "Temperature(C)"] = self.temp_history["c"]
+            self.x_range = [300, 0]
+            self.y_range = [self.data[f"Temperature({self.display_in})"].min(), self.data[f"Temperature({self.display_in})"].max()]
+
+    def shift_axis(self, amount_x, amount_y):
+        self.x_range = [self.x_range[0] + amount_x/2, self.x_range[1] + amount_x/2]
+        self.y_range = [self.y_range[0] + amount_y/2, self.y_range[1] + amount_y/2]
+        self.figure.update_xaxes(autorange=False, range=self.x_range)
+        self.figure.update_yaxes(autorange=False, range=self.y_range)
+
+    def zoom_x(self, amount):
+        self.x_range = [self.x_range[0] - amount/2, self.x_range[1] + amount/2]
+        self.figure.update_xaxes(autorange=False, range=self.x_range)
